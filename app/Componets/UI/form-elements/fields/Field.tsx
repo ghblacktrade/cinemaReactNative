b098168efb96f13a32d15663268b982a767e7b7d
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2022-11-13 12:08:36 +0300

    Field

--- app/Componets/UI/form-elements/fields/Field.tsx
+++ app/Componets/UI/form-elements/fields/Field.tsx
@@ -1,11 +1,22 @@
 import React, {FC} from 'react';
-import {View} from "react-native";
+import {Controller} from "react-hook-form";
+import {IField} from "./filed.interface";
 
-const Field :FC= () => {
+const Field = <T extends Record<string, any>>({
+                                                  control,
+                                                  rules,
+                                                  name,
+                                                  className,
+                                                  ...rest
+                                              }: IField<T>): JSX.Element => {
     return (
-        <View>
+        <Controller control={control} name={name} rules={rules} render={
+            ({field: {value, onChange, onBlur}}) => <>
 
-        </View>
+            </>
+        } />
+
+
     );
 };
 
